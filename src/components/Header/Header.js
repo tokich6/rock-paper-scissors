bfcf4ca146f1328deaec09f555a354d0012c5f20
Tonika <tokich6@gmail.com>
2021-03-03 08:32:45 -0500

    put hands in pentagon bonus shape, add mobile styles for pentagon and heading on advanced mode

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import logo_bonus from '../../assets/images/logo-bonus.svg';
 
 const Header = ({score, isAdvanced}) => {
   return (
-    <section className='heading'>
+    <section className={`heading ${!isAdvanced ? 'heading-normal' : 'heading-advanced'}`}>
       <img  src={!isAdvanced ? logo : logo_bonus} alt='rock paper scissors'/>
       <Score score={score} />
     </section>
