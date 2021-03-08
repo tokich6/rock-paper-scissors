358ec561641ab846a80afb935c060c1680fdccef
Tonika <tokich6@gmail.com>
2021-03-08 09:37:13 -0500

    add footer link to portfolio and some more corrections to smaller mobile styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,13 +3,14 @@ import Button from '../Button/Button';
 
 
 
-const Footer = ({isAdvanced, onAdvanced, onReset, onModal}) => {
+const Footer = ({ isAdvanced, onAdvanced, onReset, onModal }) => {
   return (
     <footer>
       <Button text={!isAdvanced ? 'advanced mode' : 'normal mode'} onClick={onAdvanced} />
       <div>
         <Button text='Reset score' onClick={onReset} />
         <Button text='Rules' onClick={onModal} />
+          <a href='https://tonika.tech' target='blank'> &hearts;</a>
       </div>
     </footer>
   )
